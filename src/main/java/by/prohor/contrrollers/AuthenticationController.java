cd2d733f0029125f3e0034e787a75ec37bca1a2d
igorp <igorprohorchenko@gmail.com>
2023-04-11 16:50:37 +0300

    creating the authenticationController and registerUser method

---
+++ src/main/java/by/prohor/contrrollers/AuthenticationController.java
@@ -0,0 +1,20 @@
+package by.prohor.contrrollers;
+
+import by.prohor.models.ApplicationUser;
+import by.prohor.models.RegistrationDTO;
+import by.prohor.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ApplicationUser registerUser(@RequestBody RegistrationDTO body){
+        return authenticationService.registerUser(body.getUsername(), body.getPassword());
+    }
+}
