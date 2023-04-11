df971bf4f5b377440bf94ac3cda1e02a12a3edf6
igorp <igorprohorchenko@gmail.com>
2023-04-11 16:29:03 +0300

    using the userRepository in the userService

--- src/main/java/by/prohor/service/UserService.java
+++ src/main/java/by/prohor/service/UserService.java
@@ -2,6 +2,7 @@ package by.prohor.service;
 
 import by.prohor.models.ApplicationUser;
 import by.prohor.models.Role;
+import by.prohor.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,16 +19,14 @@ public class UserService implements UserDetailsService {
     @Autowired
     private PasswordEncoder encoder;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         System.out.println("in the user details service");
 
-        if(!username.equals("Garry")) throw new UsernameNotFoundException("Not Garry");
-
-        Set<Role> roles = new HashSet<>();
-        roles.add(new Role(1, "USER"));
-
-        return new ApplicationUser(1, "Garry", encoder.encode("password"), roles);
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("user is not valid"));
     }
 }
